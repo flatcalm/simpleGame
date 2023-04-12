f07cd682f8ec2ba1fa1d287a9c1a82a9792873d0
flatcalm <a_abnc@naver.com>
2023-04-12 18:33:53 +0900

    가위바위보 게임 1일차 최종

--- Rock Paper Scissors.js
+++ Rock Paper Scissors.js
@@ -1,12 +1,13 @@
-
 // 게임에 필요한 정보를 저장할 객체
 const gameData = {
-    computerChoice : ['바위', '가위', '보'],
-    myChoice : ['바위', '가위', '보'],
-    result : '',
-    gameCount : 0,
-    winCount : 0,
-    defeatCount : 0
+    choices: ['가위', '바위', '보'],
+    computerChoice: '',
+    userChoice: '',
+    result: '',
+    gameCount: 0,
+    winCount: 0,
+    drawCount: 0,
+    defeatCount: 0
 };
 
 // 버튼에 리스너 추가
@@ -14,27 +15,104 @@ const $btn = document.getElementById('buttons');
 const $resultBtn = document.getElementById('resultBtns');
 
 $btn.addEventListener('click', e => {
-    if(e.target.matches('#rock')) {
-        console.log('바위 클릭됌!');
-    } else if(e.target.matches('#scissors')) {
-        console.log('가위 클릭됌!');
-    } else if(e.target.matches('#paper')) {
-        console.log('보 클릭됌!');
-    } 
+    if(e.target.matches('button')) {
+        // clearTimeout();
+        const decisionRn = decisionRandomChoice();
+        const decisionNum = decisionUserChoice(e.target);
+        console.log('decisionUserChoice 완료');
+        saveValue(decisionRn, decisionNum);
+        console.log('saveValue 완료');
+        judgeResult();
+        console.log('judgeResult 완료');
+        showResult();
+        console.log('showResult 완료');
+    }
+    // if (e.target.matches('#rock')) {
+    //     console.log('바위 클릭됌!');
+    // } else if (e.target.matches('#scissors')) {
+    //     console.log('가위 클릭됌!');
+    // } else if (e.target.matches('#paper')) {
+    //     console.log('보 클릭됌!');
+    // } 
     else return;
 });
 
 $resultBtn.addEventListener('click', e => {
-    if(e.target.matches('#restart')) {
+    if (e.target.matches('#restart')) {
         console.log('다시하기 클릭됌!');
-    } else if(e.target.matches('#quit')) {
+    } else if (e.target.matches('#quit')) {
         console.log('그만하기 클릭됌!');
     }
 });
 
-// 컴퓨터의 가위바위보 값을 결정할 함수
-function decisionComputerChoice() {
-    const decisionRPS = Math.floor(Math.random() * 3);
-    return decisionRPS;
+// 가위바위보 값을 결정할 난수 생성 함수
+function decisionRandomChoice() {
+    const decisionRn = Math.floor(Math.random() * 3);
+    console.log(decisionRn);
+    return decisionRn;
 }
 
+// 유저가 선택한 가위바위보 값 전달
+function decisionUserChoice(target) {
+    let decisionNum = 0;
+    if (target.matches('#scissors')) {
+        console.log('가위');
+        return decisionNum;
+    } else if (target.matches('#rock')) {
+        console.log('바위');
+        return decisionNum = 1;
+    } else if (target.matches('#paper')) {
+        console.log('보');
+        return decisionNum = 2;
+    }
+}
+
+// 객체에 결정된 값 저장
+function saveValue(decisionRn, decisionNum) {
+    gameData.computerChoice = gameData.choices[decisionRn];
+    gameData.userChoice = gameData.choices[decisionNum];
+    console.log(gameData.computerChoice);
+    console.log(gameData.userChoice);
+};
+
+// 객체에 저장된 값을 바탕으로 승부 판정
+function judgeResult() {
+    const {computerChoice, userChoice} = gameData;
+    if (userChoice === computerChoice) {
+        gameData.result = '비겼습니다.';
+        gameData.gameCount++;
+        gameData.drawCount++;
+    } else if (userChoice === '바위' && computerChoice === '가위' || userChoice === '가위' && computerChoice === '보' ||
+        userChoice === '보' && computerChoice === '바위') {
+        gameData.result = '이겼습니다!';
+        gameData.gameCount++;
+        gameData.winCount++;
+    } else {
+        gameData.result = '졌습니다...';
+        gameData.gameCount++;
+        gameData.defeatCount++;
+    }
+}
+
+// 결과값 출력
+function showResult() {
+    const $buttons = document.querySelector('#buttons');
+    const $result = document.querySelector('.result');
+    
+    const $comImg = document.createElement('div');
+    const $userImg = document.createElement('div');
+    $comImg.style.background = 'url(./images/' + gameData.computerChoice + '.png)';
+    $userImg.style.background = 'url(./images/' + gameData.userChoice + '.png)';
+
+
+    $buttons.classList.toggle('hide');
+    $result.classList.toggle('hide');
+
+
+}
+
+(function() {
+    // setTimeout(() => {
+        // 5초동안 선택하지 않으면 실행할 이벤트 함수
+    // }, 5000);
+})();
